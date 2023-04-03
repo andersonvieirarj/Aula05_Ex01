b855397a748395d449eaffd84eb964d4d3368979
andersonvieirarj <andersonvieirarj@gmail.com>
2023-04-03 17:39:24 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    // Calcular a média de idade de dez pessoas
+    double i;
+    double idade;
+    double media=0;
+    Scanner entrada = new Scanner (System.in);
+    for (i=1;i<=10;i++) {
+        System.out.println("Digite a idade: ");
+        idade = entrada.nextInt();
+        media = media + idade;
+      }
+    media = media/i;
+    System.out.println("Média das idades" + media);
+    }
+}
